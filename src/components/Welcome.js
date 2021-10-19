c6f67ce28bed8b69faf78f965d62ecda21a728ac
matthiashaefeli <haefeli.matthias2gmail.com>
2021-10-19 12:01:05 -0500

    header links, welcome component, router

---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <h3>
+      Welcome! Sign Up or Sign In.
+    </h3>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
 import App from './components/App';
+import Welcome from './components/Welcome'
 
 ReactDom.render(
-  <App />,
+  <BrowserRouter>
+    <App>
+      <Route path="/" exact component={Welcome} />
+    </App>
+  </BrowserRouter>,
   document.querySelector('#root')
 )
